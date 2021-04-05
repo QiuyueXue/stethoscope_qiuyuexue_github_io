b624240fc9e3c7e30782a5e97bae7b63a5e5347f
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2021-04-04 20:13:18 -0700

    set echoCancellation and noiseSuppression false

--- scripts/app.js
+++ scripts/app.js
@@ -174,8 +174,8 @@ function start() {
   const constraints = {
     audio: {
       deviceId: audioSource ? {exact: audioSource} : undefined,
-      echoCancellation: true,
-      noiseSuppression: true,
+      echoCancellation: false,
+      noiseSuppression: false,
       autoGainControl: true,
     }
   };
