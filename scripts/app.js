1fc9474531e76278297ffccf3a056ea3fbe96452
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2021-04-13 16:27:38 -0700

    set echoCancellation false

--- scripts/app.js
+++ scripts/app.js
@@ -174,7 +174,7 @@ function start() {
     audio: {
       deviceId: audioSource ? {exact: audioSource} : undefined,
       echoCancellation: false,
-      noiseSuppression: false
+      noiseSuppression: true
     }
   };
   
