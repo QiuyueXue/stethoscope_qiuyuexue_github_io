b3285fbf89d8796acf4f2efe6fe1240a46df8395
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2021-06-04 14:17:00 -0700

    find peaks naive version

--- scripts/app.js
+++ scripts/app.js
@@ -154,13 +154,49 @@ function visualize(stream) {
   }
 
   function compute_peaks(){
-    var ricker = d3_peaks.ricker;
-    var findPeaks = d3_peaks.findPeaks()
-      .kernel(ricker)
-      .gapThreshold(4000)
-    var peaks = findPeaks(graphWindowData);
-    console.log(peaks);
+    var peaks = getPeaksAtThreshold(graphWindowData, 0.1*Math.max(graphWindowData));
+    var intervalCounts = countIntervalsBetweenNearbyPeaks(peaks);
+    
+    // var ricker = d3_peaks.ricker;
+    // var findPeaks = d3_peaks.findPeaks()
+    //   .kernel(ricker)
+    //   .gapThreshold(4000)
+    // var peaks = findPeaks(graphWindowData);
+    console.log(intervalCounts);
   }
+  function getPeaksAtThreshold(data, threshold) {
+    var peaksArray = [];
+    var length = data.length;
+    for (var i = 0; i < length;) {
+      if (data[i] > threshold) {
+        peaksArray.push(i);
+        // Skip forward ~ 1/4s to get past this peak.
+        i += 10000;
+      }
+      i++;
+    }
+    return peaksArray;
+  }
+  function countIntervalsBetweenNearbyPeaks(peaks) {
+  var intervalCounts = [];
+  peaks.forEach(function(peak, index) {
+    for (var i = 0; i < 10; i++) {
+      var interval = peaks[index + i] - peak;
+      var foundInterval = intervalCounts.some(function(intervalCount) {
+        if (intervalCount.interval === interval) return intervalCount.count++;
+      });
+      //Additional checks to avoid infinite loops in later processing
+      if (!isNaN(interval) && interval !== 0 && !foundInterval) {
+        intervalCounts.push({
+          interval: interval,
+          count: 1
+        });
+      }
+    }
+  });
+  return intervalCounts;
+}
+
 }
 
 function gotStream(stream) {
